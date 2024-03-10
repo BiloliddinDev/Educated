80b0f425f9ee73f8c4145c62a987073247d6ae52
Ravshanbek <oravshan26@gmail.com>
2024-03-11 00:20:06 +0500

    added axios

--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -1,76 +1,70 @@
-import React, { useState } from 'react';
-import { Form, Input, Button } from 'antd';
-import { Link, useNavigate } from 'react-router-dom';
+import { Button, Form, Input } from 'antd'
+import axios from 'axios'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [error, setError] = useState<string | null>(null);
+	const navigate = useNavigate()
+	const [error, setError] = useState<string | null>(null)
 
-    const onFinish = async (values: { email: string; password: string }) => {
-        try {
-            const response = await fetch(
-                'https://e-backend-8z4t.onrender.com/auth/login',
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(values),
-                }
-            );
+	const onFinish = async (values: { email: string; password: string }) => {
+		try {
+			const response = await axios.post(
+				'https://e-backend-8z4t.onrender.com/auth/login',
+				values
+			)
 
-            if (!response.ok) {
-                throw new Error('Invalid credentials');
-            }
+			if (!response.status === 200) {
+				throw new Error('Invalid credentials')
+			}
 
-            const data = await response.json();
-            console.log('Login successful:', data);
-            navigate('/');
-        } catch (error) {
-            setError(error.message);
-        }
-    };
+			console.log('Login successful:', response.data)
+			navigate('/')
+		} catch (error) {
+			setError(error.message)
+		}
+	}
 
-    const onFinishFailed = (errorInfo: any) => {
-        console.log('Failed:', errorInfo);
-    };
+	const onFinishFailed = (errorInfo: any) => {
+		console.log('Failed:', errorInfo)
+	}
 
-    return (
-        <div className='flex flex-col justify-center items-center h-screen'>
-            <h1 className='text-center text-4xl my-5'>Login page</h1>
-            <Form
-                name='basic'
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete='off'
-                className='flex flex-col justify-center w-[500px]'
-            >
-                <h1 className='text-1xl mb-2'>Email</h1>
-                <Form.Item
-                    name='email'
-                    rules={[{ required: true, message: 'Please input your email!' }]}
-                >
-                    <Input size='large' />
-                </Form.Item>
-                <h1 className='text-1xl mb-2'>Password</h1>
-                <Form.Item
-                    name='password'
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                >
-                    <Input.Password size='large' />
-                </Form.Item>
+	return (
+		<div className='flex flex-col justify-center items-center h-screen'>
+			<h1 className='text-center text-4xl my-5'>Login page</h1>
+			<Form
+				name='basic'
+				initialValues={{ remember: true }}
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}
+				autoComplete='off'
+				className='flex flex-col justify-center w-[500px]'
+			>
+				<h1 className='text-1xl mb-2'>Email</h1>
+				<Form.Item
+					name='email'
+					rules={[{ required: true, message: 'Please input your email!' }]}
+				>
+					<Input size='large' />
+				</Form.Item>
+				<h1 className='text-1xl mb-2'>Password</h1>
+				<Form.Item
+					name='password'
+					rules={[{ required: true, message: 'Please input your password!' }]}
+				>
+					<Input.Password size='large' />
+				</Form.Item>
 
-                <Button size={'large'} type='primary' htmlType='submit'>
-                    Submit
-                </Button>
-                <Link to={'/'}>
-                    <Button type='link'>Go to home</Button>
-                </Link>
-                {error && <p>{error}</p>}
-            </Form>
-        </div>
-    );
-};
+				<Button size={'large'} type='primary' htmlType='submit'>
+					Submit
+				</Button>
+				<Link to={'/'}>
+					<Button type='link'>Go to home</Button>
+				</Link>
+				{error && <p>{error}</p>}
+			</Form>
+		</div>
+	)
+}
 
-export default Login;
+export default Login
