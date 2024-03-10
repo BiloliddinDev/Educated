02305dfceff01a56ddd2051e088bcdf95a751547
Ravshanbek <oravshan26@gmail.com>
2024-03-11 01:14:03 +0500

    fix it role

--- src/auth/index.tsx
+++ src/auth/index.tsx
@@ -1,50 +1,46 @@
-import { Navigate } from "react-router-dom";
+import { Navigate } from 'react-router-dom'
 
 const user = {
-  simple: "ghost",
-  student: "student",
-  admin: "admin",
-  teacher: "teacher",
-};
-
-const curentUser = localStorage.getItem("role");
+	simple: 'ghost',
+	student: 'student',
+	admin: 'admin',
+	teacher: 'teacher',
+}
 
 export const PuplickRoute = ({ children }: any) => {
-  return <>{children}</>;
-};
+	return <>{children}</>
+}
 
 export const StudentRoute = ({ children }: any) => {
-  if (
-    curentUser === user.student &&
-    curentUser !== user.admin &&
-    curentUser !== user.teacher
-  ) {
-    return <>{children}</>;
-  } else {
-    return <Navigate to={"/"} />;
-  }
-};
+	const curentUser = localStorage.getItem('role')
+
+	if (
+		curentUser === user.student &&
+		curentUser !== user.admin &&
+		curentUser !== user.teacher
+	) {
+		return <>{children}</>
+	} else {
+		return <Navigate to={'/'} />
+	}
+}
 
 export const TeacherRoute = ({ children }: any) => {
-  if (
-    curentUser === user.teacher
-    // curentUser !== user.admin &&
-    // curentUser !== user.student
-  ) {
-    return <>{children}</>;
-  } else {
-    return <Navigate to={"/"} />;
-  }
-};
+	const curentUser = localStorage.getItem('role')
+
+	if (curentUser === user.teacher) {
+		return <>{children}</>
+	} else {
+		return <Navigate to={'/'} />
+	}
+}
 
 export const AdminRoute = ({ children }: any) => {
-  if (
-    curentUser === user.admin
-    // curentUser !== user.teacher &&
-    // curentUser !== user.student
-  ) {
-    return <>{children}</>;
-  } else {
-    return <Navigate to={"/"} />;
-  }
-};
+	const curentUser = localStorage.getItem('role')
+
+	if (curentUser === user.admin) {
+		return <>{children}</>
+	} else {
+		return <Navigate to={'/'} />
+	}
+}
