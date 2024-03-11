459074d8509b19609d0f22aa5a1c7aca9d911cda
Biloliddin_dev <bilol9876@gmail.com>
2024-03-11 19:21:33 +0500

    add admin page

--- src/auth/index.tsx
+++ src/auth/index.tsx
@@ -1,46 +1,46 @@
-import { Navigate } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 
 const user = {
-	simple: 'ghost',
-	student: 'student',
-	admin: 'admin',
-	teacher: 'teacher',
-}
+  simple: "ghost",
+  student: "student",
+  admin: "admin",
+  teacher: "teacher",
+};
 
 export const PuplickRoute = ({ children }: any) => {
-	return <>{children}</>
-}
+  return <>{children}</>;
+};
 
 export const StudentRoute = ({ children }: any) => {
-	const curentUser = localStorage.getItem('role')
+  const curentUser = localStorage.getItem("role");
 
-	if (
-		curentUser === user.student &&
-		curentUser !== user.admin &&
-		curentUser !== user.teacher
-	) {
-		return <>{children}</>
-	} else {
-		return <Navigate to={'/'} />
-	}
-}
+  if (
+    curentUser === user.student &&
+    curentUser !== user.admin &&
+    curentUser !== user.teacher
+  ) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to={"/"} />;
+  }
+};
 
 export const TeacherRoute = ({ children }: any) => {
-	const curentUser = localStorage.getItem('role')
+  const curentUser = localStorage.getItem("role");
 
-	if (curentUser === user.teacher) {
-		return <>{children}</>
-	} else {
-		return <Navigate to={'/'} />
-	}
-}
+  if (curentUser === user.teacher) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to={"/"} />;
+  }
+};
 
 export const AdminRoute = ({ children }: any) => {
-	const curentUser = localStorage.getItem('role')
+  const curentUser = localStorage.getItem("role");
 
-	if (curentUser === user.admin) {
-		return <>{children}</>
-	} else {
-		return <Navigate to={'/'} />
-	}
-}
+  if (curentUser === user.admin) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to={"/"} />;
+  }
+};
--- src/page/admin/index.tsx
+++ src/page/admin/index.tsx
@@ -1,7 +1,24 @@
+import Madal from "@/components/shared/madal";
+import { Button } from "@/components/ui/button";
+import { useFolder } from "@/utils/zuztand";
 import React from "react";
 
 const Admin = () => {
-  return <div className="mt-[60px]">Admin</div>;
+  const { onOpen } = useFolder();
+  return (
+    <div className="mt-[60px] w-full flex">
+      <div className="bg-slate-400 w-[200px] h-[682px] py-10 px-4 flex flex-col gap-4">
+        <Button onClick={() => onOpen()}>Create Teacher</Button>
+        <Button onClick={() => onOpen()}>Create Group</Button>
+        <Button onClick={() => onOpen()}>Create Student</Button>
+      </div>
+      <h1 className="w-full">Admin page</h1>
+
+      <Madal>
+        <h1>Salom Dunyo</h1>
+      </Madal>
+    </div>
+  );
 };
 
 export default Admin;
