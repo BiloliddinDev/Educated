17262a0e8f5b972e9b88a26d9a366dfc21663e20
Ravshanbek <oravshan26@gmail.com>
2024-03-20 13:12:45 +0500

    new design

--- src/page/teacher/pages/courses/index.tsx
+++ src/page/teacher/pages/courses/index.tsx
@@ -1,100 +1,109 @@
-import Madal from "@/components/shared/madal";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { baseurl } from "@/utils/axios";
-import { useFolder } from "@/utils/zuztand";
+import Madal from '@/components/shared/madal'
+import { Button } from '@/components/ui/button'
+import { Label } from '@/components/ui/label'
+import { baseurl } from '@/utils/axios'
+import { useFolder } from '@/utils/zuztand'
 import {
-  Avatar,
-  DatePicker,
-  FloatButton,
-  Form,
-  Input,
-  Select,
-  Upload,
-  UploadProps,
-  message,
-} from "antd";
-import Dragger from "antd/es/upload/Dragger";
-import { File, Pencil } from "lucide-react";
-import { useEffect, useState } from "react";
-import { FaFile } from "react-icons/fa";
+	Avatar,
+	DatePicker,
+	FloatButton,
+	Form,
+	Input,
+	Select,
+	message,
+} from 'antd'
+import { Pencil } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
-interface teacher {
-  user: {
-    email: string;
-    name: string;
-    role: string;
-    createdAt: string;
-    __v: number;
-    _id: string;
-    profileImage: {
-      path: string;
-    };
-  };
+interface Teacher {
+	user: {
+		email: string
+		name: string
+		role: string
+		createdAt: string
+		__v: number
+		_id: string
+		profileImage: {
+			path: string
+		}
+	}
 }
 
-interface imae {
-  image: any;
-}
+const Courses = () => {
+	const { onOpen, onClose } = useFolder()
+	const token1 = localStorage.getItem('token')
 
-const Curses = () => {
-  const { onOpen } = useFolder();
-  const token1 = localStorage.getItem("token");
+	const [data, setData] = useState<Teacher>()
+	const [form] = Form.useForm()
 
-  const [data, setData] = useState<teacher>();
+	useEffect(() => {
+		baseurl.get('/profile').then(res => setData(res.data))
+	}, [])
 
-  useEffect(() => {
-    baseurl.get("/profile").then((res) => setData(res.data));
-  }, []);
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const files: any = e.target.files
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files: any = e.target.files;
+		baseurl
+			.put(`/profile/image`, {
+				profileImage: files[0],
+			})
+			.then(res => {
+				setData(res.data)
+				message.success('Updated profile image')
+			})
+	}
 
-    baseurl
-      .put(`/profile/image`, {
-        profileImage: files[0],
-      })
-      .then((res) => console.log(res));
-  };
+	const onFinish = async (values: any) => {
+		baseurl
+			.put(`/profile`, {
+				user: {
+					...values,
+				},
+			})
+			.then(res => {
+				setData(res.data)
+				onClose()
+				form.resetFields()
+			})
+	}
 
-  const onFinish = async (values: any) => {
-    baseurl
-      .put(`/profile`, {
-        user: {
-          ...values,
-        },
-      })
-      .then((res) => console.log(res.data, "Zapros jonatildi"));
-  };
+	return (
+		<div className='relative'>
+			<div className='p-2 bg-slate-500 h-[200px] flex justify-center items-center relative'>
+				<Avatar
+					src={data?.user.profileImage?.path}
+					className='absolute bottom-[-100px] w-[200px] h-[200px] bg-red-600 rounded-full border-4 border-white'
+					size={'large'}
+				/>
+			</div>
+			<div className='mt-[150px] flex justify-center flex-col items-center gap-4'>
+				<h1 className='text-4xl'>{data?.user?.email}</h1>
+				<h1 className='text-4xl'>{data?.user?.name}</h1>
+				<h1 className='text-4xl uppercase'>{data?.user?.role}</h1>
+				<div className='grid w-full max-w-sm items-center gap-1.5'>
+					<Label htmlFor='picture' className='text-lg font-bold'>
+						
+					</Label>
+					<label className='cursor-pointer bg-gray-100 py-2 px-4 rounded-md border border-gray-300 hover:bg-gray-200'>
+						<input
+							onChange={onChange}
+							id='picture'
+							type='file'
+							className='hidden'
+						/>
+						Upload Image
+					</label>
+				</div>
+			</div>
 
-  return (
-    <div>
-      <div className="p-2 bg-slate-500 h-[200px] flex justify-center relative">
-        <Avatar
-          src={data?.user.profileImage?.path}
-          className="absolute bottom-[-100px] w-[200px] h-[200px] bg-red-600"
-          size={"large"}
-        ></Avatar>
-      </div>
-      <div className="mt-[150px] flex justify-center flex-col items-center gap-4">
-        <h1 className="text-4xl">{data?.user?.email}</h1>
-        <h1 className="text-4xl">{data?.user?.name}</h1>
-        <h1 className="text-4xl uppercase">{data?.user?.role}</h1>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="picture">Picture</Label>
-          <Input onChange={onChange} id="picture" type="file" />
-        </div>
-      </div>
-
-      <FloatButton
-        onClick={() => onOpen()}
-        className="w-[100px] h-[30px]"
-        icon={<Pencil />}
-        type="primary"
-        shape="square"
-        style={{ right: 24 }}
-      />
-      <Madal>
+			<FloatButton
+				onClick={() => onOpen()}
+				className='w-[100px] h-[30px] absolute top-[20px] right-[20px] rounded-full bg-blue-500 shadow-lg'
+				icon={<Pencil />}
+				type='primary'
+				shape='square'
+			/>
+			 <Madal>
         <div className="flex flex-col justify-center items-center">
           <Form
             name="basic"
@@ -150,8 +159,9 @@ const Curses = () => {
           </Form>
         </div>
       </Madal>
-    </div>
-  );
-};
 
-export default Curses;
+		</div>
+	)
+}
+
+export default Courses
