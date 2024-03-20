793419aa8d5d4d333d96975614b4569981a2c817
Ravshanbek <oravshan26@gmail.com>
2024-03-20 13:45:35 +0500

    fix it

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,14 +1,12 @@
-import axios from "axios";
+import axios from 'axios'
 
-const token1 = localStorage.getItem("token");
+const token1 = localStorage.getItem('token')
 
 export const baseurl = axios.create({
-  baseURL: "https://e-center-backend-gbuy.onrender.com/api",
-  timeout: 5000,
+	baseURL: 'https://e-center-backend-gbuy.onrender.com/api',
+	timeout: 5000,
 
-  headers: {
-    Authorization: `Bearer ${token1}`,
-    "Content-Type": "multipart/form-data",
-    // "Content-Type": "multipart/form-data",
-  },
-});
+	headers: {
+		Authorization: `Bearer ${token1}`,
+	},
+})
